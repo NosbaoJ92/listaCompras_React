a49c9649b86a435399f35b3902e896d606d746c5
NosbaoJ92 <joabson.nogueira@hotmail.com>
2024-09-02 18:45:03 -0300

    terceiro commit

--- src/ValorMaximo.jsx
+++ src/ValorMaximo.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-import { Menu, MenuItem, IconButton, Switch } from '@mui/material';
+import { Menu, MenuItem, IconButton, Switch, Modal, Box, Button, TextField } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import EditIcon from '@mui/icons-material/Edit';
 
 const ValorMaximo = () => {
   const [produtos, setProdutos] = useState([]);
@@ -63,6 +64,7 @@ const ValorMaximo = () => {
     setValorProduto(produto.valor.toString());
     setQuantidadeProduto(produto.quantidade.toString());
     setEditandoIndex(index);
+    setModalAberto(true);
   };
 
   const handleDeleteProduto = (index) => {
@@ -186,7 +188,7 @@ const ValorMaximo = () => {
   };
 
   return (
-    <div className={`container mx-auto p-4 h-screen w-screen ${modoNoturno ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
+    <div className={`container mx-auto p-4 h-screen w-screen flex flex-col justify-between ${modoNoturno ? 'bg-gray-800 text-white' : 'bg-white text-black'}`}>
       <div className="flex w-full justify-between items-center mb-4">
         <IconButton onClick={handleMenuClick}>
           <MenuIcon />
@@ -197,76 +199,70 @@ const ValorMaximo = () => {
           onClose={handleMenuClose}
         >
           <MenuItem onClick={handleMenuClose}>
-          <Switch checked={modoNoturno} onChange={alternarModoNoturno} /><p>Dark Mode</p>
+            <Switch checked={modoNoturno} onChange={alternarModoNoturno} /><p>Dark Mode</p>
           </MenuItem>
           <MenuItem onClick={handleMenuClose}>Opção 2</MenuItem>
           <MenuItem onClick={handleMenuClose}>Opção 3</MenuItem>
         </Menu>
         <h1 className="text-xl font-bold">Estipular Valor Máximo de Gasto</h1>
-        
-      </div>
-      <div className="mb-4">
-        <label className="block mb-2">Valor Máximo (R$)</label>
-        <div className="flex flex-col sm:flex-row gap-2">
-          <input
-            type="number"
-            placeholder="Ex: 100.00"
-            value={valorMaximo}
-            onChange={(e) => setValorMaximo(e.target.value)}
-            className={`border rounded-md p-2 flex-1 ${modoNoturno ? 'bg-gray-700 text-white' : 'text-black'}`}
-          />
-          <button
-            onClick={confirmarValorMaximo}
-            className="bg-green-500 text-white rounded-md p-2 hover:bg-green-600"
-          >
-            Confirmar Valor Máximo
-          </button>
-        </div>
-      </div>
-
-      <div className="flex gap-2 mb-4 flex-wrap">
-        <input
-          type="text"
-          placeholder="Nome do Produto"
-          value={nomeProduto}
-          onChange={(e) => setNomeProduto(e.target.value)}
-          onKeyDown={handleKeyDown}
-          className={`border rounded-md p-2 flex w-full ${modoNoturno ? 'bg-gray-700 text-white' : 'text-black'}`}
-        />
-        <input
-          type="number"
-          placeholder="Valor (R$)"
-          value={valorProduto}
-          onChange={(e) => setValorProduto(e.target.value)}
-          onKeyDown={handleKeyDown}
-          className={`border rounded-md p-2 flex w-full ${modoNoturno ? 'bg-gray-700 text-white' : 'text-black'}`}
-        />
-        <input
-          type="number"
-          placeholder="Quantidade"
-          value={quantidadeProduto}
-          onChange={(e) => setQuantidadeProduto(e.target.value)}
-          onKeyDown={handleKeyDown}
-          className={`border rounded-md p-2 flex w-full ${modoNoturno ? 'bg-gray-700 text-white' : 'text-black'}`}
-        />
-        <button
-          onClick={handleAddProduto}
-          className="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-600 w-full"
+        <div className='invisible'>
+        <IconButton onClick={handleMenuClick}>
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
         >
-          {editandoIndex !== null ? 'Salvar Alterações' : 'Adicionar Produto'}
-        </button>
+          <MenuItem onClick={handleMenuClose}>
+            <Switch checked={modoNoturno} onChange={alternarModoNoturno} /><p>Dark Mode</p>
+          </MenuItem>
+          <MenuItem onClick={handleMenuClose}>Opção 2</MenuItem>
+          <MenuItem onClick={handleMenuClose}>Opção 3</MenuItem>
+        </Menu>
+        </div>
       </div>
 
-      {erro && <p className="text-red-500 mb-4">{erro}</p>}
-
-      <h2 className="text-xl font-bold mb-2">Lista de Compras</h2>
-      <div className="bg-yellow-100 p-4 rounded-md mb-4">
-          <p className="text-yellow-800 font-bold">Valor Máximo Confirmado: R$ {valorMaximoConfirmado}</p>
+      <div className="mb-4">
+        <label className="block mb-2 text-center">Valor Máximo (R$)</label>
+        <div className="flex flex-col sm:flex-row gap-2">
+          {valorMaximoConfirmado ? (
+            <IconButton onClick={() => setValorMaximoConfirmado(null)}>
+              <EditIcon />
+            </IconButton>
+          ) : (
+            <div className='flex flex-wrap gap-2'>
+              <input
+                type="number"
+                placeholder="Ex: 100.00"
+                value={valorMaximo}
+                onChange={(e) => setValorMaximo(e.target.value)}
+                className={`border rounded-md p-2 flex-1 ${modoNoturno ? 'bg-gray-700 text-white' : 'text-black'}`}
+              />
+              <div className="flex w-full  justify-between gap-2">
+                <button
+                  onClick={confirmarValorMaximo}
+                  className="bg-green-500 w-6/12 text-white rounded-md p-2 hover:bg-green-600"
+                >
+                  Confirmar Valor Máximo
+                </button>
+                {/* <button
+                onClick={() => setValorMaximo('')}
+                className="bg-red-500 w-6/12 text-white rounded-md p-2 hover:bg-red-600"
+                >
+                Cancelar
+                </button> */}
+              </div>
+            </div>
+          )}
         </div>
-      <div className="overflow-x-auto mb-4">
+      </div>
+
+      
+      <div className="overflow-x-auto mb-4 h-full">
         <table className="w-full min-w-max table-auto">
           <thead>
-            <tr className="bg-gray-400">
+            <tr className="bg-gray-300">
               <th className="px-4 py-2 border-2 border-black text-black">Nome</th>
               <th className="px-4 py-2 border-2 border-black text-black">Valor Unitário</th>
               <th className="px-4 py-2 border-2 border-black text-black">Quantidade</th>
@@ -279,17 +275,17 @@ const ValorMaximo = () => {
               <tr key={index} className="bg-white text-center border-b text-black">
                 <td className="px-4 py-2 border-2 border-black">{produto.nome}</td>
                 <td className="px-4 py-2 border-2 border-black">R$ {produto.valor.toFixed(2)}</td>
-                <td className="px-4 py-2 border-2 border-black">
+                <td className="p-2 py-2 border-2 border-black flex justify-between items-center">
                   <button
                     onClick={() => handleDecrementarQuantidade(index)}
-                    className="bg-red-500 text-white rounded-md px-2 hover:bg-red-600"
+                    className="bg-red-500 text-white rounded-md p-2 hover:bg-red-600"
                   >
                     -
                   </button>
                   <span className="mx-2">{produto.quantidade}</span>
                   <button
                   onClick={() => handleIncrementarQuantidade(index)}
-                  className="bg-green-500 text-white rounded-md px-2 hover:bg-green-600"
+                  className="bg-green-500 text-white rounded-md p-2 hover:bg-green-600"
                   >
                   +
                   </button>
@@ -314,23 +310,81 @@ const ValorMaximo = () => {
           </tbody>
         </table>
       </div>
-
-      <div className="flex justify-between">
-        <div className="">
-        <p>Total da Compra: R$ {calcularTotalCompra().toFixed(2)}</p>
-        {valorMaximoConfirmadoNumerico > 0 && (
-          <p>Valor Restante: R$ {calcularValorRestante().toFixed(2)}</p>
-        )}
-        </div>
-        <div className="">
-        <button
-          onClick={gerarPDF}
-          className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600"
-        >
-          Gerar PDF
-        </button>
-        </div>
+      <div className="mb-4 flex justify-center items-center">
+        <Button variant="contained" color="primary" onClick={() => setModalAberto(true)}>
+          Adicionar Produto
+        </Button>
+        {erro && <p className="text-red-500">{erro}</p>}
       </div>
+      <div className="bg-yellow-100 p-4 rounded-md mb-4">
+        <p className="text-yellow-800 font-bold text-center">Valor Máximo Confirmado: R$ {valorMaximoConfirmado}</p>
+      </div>
+
+      <div className="flex justify-around">
+        <p>Total da Compra: R$ {calcularTotalCompra().toFixed(2)}</p>
+        {valorMaximoConfirmado && (
+          <p>
+            {calcularTotalCompra() > valorMaximoConfirmadoNumerico ? (
+              <span className="text-red-500">
+                Excedeu o valor máximo em R$ {(calcularTotalCompra() - valorMaximoConfirmadoNumerico).toFixed(2)}
+              </span>
+            ) : (
+              <>
+                <span>Restante: R$ {calcularValorRestante().toFixed(2)}</span>
+              </>
+            )}
+          </p>
+        )}
+      </div>
+
+      <button
+        onClick={gerarPDF}
+        className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600"
+      >
+        Gerar PDF
+      </button>
+
+      <Modal
+        open={modalAberto}
+        onClose={handleModalRevisar}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box className="bg-white rounded p-4 max-w-sm mx-auto mt-40">
+          <h2 id="modal-title" className="text-xl font-bold mb-4 text-black text-center">Adicionar Produto</h2>
+          <TextField
+            label="Nome do Produto"
+            value={nomeProduto}
+            onChange={(e) => setNomeProduto(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Valor do Produto"
+            type="number"
+            value={valorProduto}
+            onChange={(e) => setValorProduto(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Quantidade"
+            type="number"
+            value={quantidadeProduto}
+            onChange={(e) => setQuantidadeProduto(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <Box mt={2} display="flex" justifyContent="space-between">
+            <Button variant="contained" color="primary" onClick={handleAddProduto}>
+              Confirmar
+            </Button>
+            <Button variant="contained" color="secondary" onClick={handleModalRevisar}>
+              Cancelar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </div>
   );
 };
